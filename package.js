ae8ac8fb44b5756be254a0b65c4f23c1ca9ee05d
Ronald <rolly.maduk@gmail.com>
2014-09-03 15:22:00 +0100

    modified and change files

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   summary: "Simple jquery.sparkline package",
-  version: "1.3.0",
+  version: "1.3.1",
   git:"https://github.com/rollymaduk/jqsparkline.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
-  api.addFiles('lib/sparkline.html','client');
-  api.addFiles('lib/jquery.sparkline.min.js','client');
+  api.use(['jquery','underscore']);
+  api.addFiles('sparkline.html','client');
+  api.addFiles('lib/jquery.sparkline.js','client');
 });
 
 Package.onTest(function(api) {
